e6e0b6d6f1e2a2cceeb7e66aacdcae4d62bdb8c3
Peter Coker <petercoker@protonmail.com>
2018-07-10 17:13:18 +0100

    complete the class learn

--- csharp-basic/classes/classes/Program.cs
+++ csharp-basic/classes/classes/Program.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿
+using System;
+using classes.math;
 
 namespace classes
 {
+    //public class Person
+    //{
+    //    public string FirstName;
+    //    public string LastName;
+
+    //    public void Introduce()
+    //    {
+    //        //Console.WriteLine("My name is " + FirstName + " " + LastName );
+    //        Console.WriteLine("My name is {0} {1}", FirstName, LastName);
+    //    }
+    //}
+
     class Program
     {
         static void Main(string[] args)
         {
+            Person peter = new Person(); //Non-Primitive needs to be give memory to it
+            peter.FirstName = "Peter";
+            peter.LastName = "Coker";
+            peter.Introduce();
+
+            Person scott = new Person();
+            scott.FirstName = "scott";
+            scott.LastName = "brown";
+            scott.Introduce();
+
+            //int result = Calculator.Add(1, 2); //If you were to add the static Modifier
+
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(1, 2);
+            Console.WriteLine("Calculator result is: " + result);
         }
     }
 }
---
+++ csharp-basic/classes/classes/math/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace classes.math
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        //public static int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+    }
+}
