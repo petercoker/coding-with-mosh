b607de91657dc21650e27ecf790d97c63684c642
Peter Coker <petercoker@protonmail.com>
2018-07-16 15:40:58 +0100

    array tutorial exercise

---
+++ csharp-basic/arrays/arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int [3];
+            //var numbers = new int [3]; //make the code shorter
+
+            numbers[0] = 1;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            bool[] flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[02]);
+            
+
+            string[] names = new string[3] {"Bart", "Lisa", "jack"};
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[02]);
+        }
+    }
+}
+ 
