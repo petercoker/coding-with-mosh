aa874a574ff3bcb0577e86664474b7acd39b11e1
Peter Coker <petercoker@protonmail.com>
2018-07-27 16:46:37 +0100

    completed exercise

--- csharp-basic/mosh-csharp-basic/mosh-csharp-basic/Program.cs
+++ csharp-basic/mosh-csharp-basic/mosh-csharp-basic/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using mosh_csharp_basic.primitive;
 using mosh_csharp_basic.nonprimitive;
+using mosh_csharp_basic.nonprimitive.classes;
 
 namespace mosh_csharp_basic
 {
@@ -26,6 +27,8 @@ namespace mosh_csharp_basic
             //CSharpArrays outPutCSharpArrays = new CSharpArrays();
             //outPutCSharpArrays.CSharpArray();
 
+
+            //CLASS *********
             //Created objects
             //Person peter = new Person(); //Non-Primitive needs to be give memory to it
             //peter.FirstName = "Peter";
@@ -55,24 +58,70 @@ namespace mosh_csharp_basic
 
             ////int result = Calculator.Add(1, 2); //If you were to add the static Modifier
 
-            StructPerson peter = new StructPerson(); //Non-Primitive needs to be give memory to it
-            peter.FirstName = "Peter";
-            peter.LastName = "Coker";
-            peter.Introduce();
+
+
+            //STUCT ********
+            //StructPerson peter = new StructPerson(); //Non-Primitive needs to be give memory to it
+            //peter.FirstName = "Peter";
+            //peter.LastName = "Coker";
+            //peter.Introduce();
 
 
 
-            //Method
-            StructCalculator calculator = new StructCalculator();
-            int result = calculator.Add(1, 2);
-            Console.WriteLine("Calculator result is: " + result);
+            ////Method
+            //StructCalculator calculator = new StructCalculator();
+            //int result = calculator.Add(1, 2);
+            //Console.WriteLine("Calculator result is: " + result);
 
             //int result = Calculator.Add(1, 2); //If you were to add the static Modifier
 
+            //ENUMS*****************
+            //ShippingMethod method = ShippingMethod.Express;
+            //Console.WriteLine((int)method);
 
+            //int methodId = 3;
+            //Console.WriteLine((ShippingMethod)methodId);
 
+            //Console.WriteLine(method.ToString()); //Console.WriteLine by default always call the ToString() method on any vaule to pass through it 
+            //Console.WriteLine(method); //In case you're not convert using Console.WriteLine and what to explitly convert enum to a string than use the ToString() method
+
+            //string methodName = "Enum";
+
+            //ShippingMethod shippingMethod = (ShippingMethod) Enum.Parse(typeof(ShippingMethod), methodName);
+
+            //Vaule Types ****
+            //int a = 10;
+            //int b = a;
+            //b++;
+            //Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+
+            ////Reference Types ****
+            //int[] array1 = new int[3] {1, 2, 3};
+            //int[] array2 = array1;
+
+            //array2[0] = 0;
+            //Console.WriteLine(string.Format("array1[0]: {0}, array[0]: {1}", array1[0], array2[0]));
+
+            int number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            ReferenceType referenceType = new ReferenceType() {Age = 20};
+            MakeOld(referenceType);
+            Console.WriteLine(referenceType.Age);
         }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(ReferenceType referenceType)
+        {
+            referenceType.Age += 10;
+        }
+
     }
 
-   
+
 }
