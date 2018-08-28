dab75136bf743e8fd4ff9bca90dba172e489b372
Peter Coker <petercoker@protonmail.com>
2018-08-28 18:07:59 +0100

    completed exercise

--- csharp-basic/mosh-csharp-basic/mosh-csharp-basic/arrys_and_lists/array_list_exercise.cs
+++ csharp-basic/mosh-csharp-basic/mosh-csharp-basic/arrys_and_lists/array_list_exercise.cs
@@ -77,6 +77,28 @@ namespace mosh_csharp_basic.arrys_and_lists
             //    Console.WriteLine();
 
 
+            //2 - Write a program and ask the user to enter their name. 
+            //    Use an array to reverse the name and then store the result in a new string.Display the reversed name on the console.
+
+
+
+            //3 - Write a program and ask the user to enter 5 numbers.
+            //    If a number has been previously entered, display an error message and ask the user to re-try. 
+            //    Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
+
+
+
+            //4 - Write a program and ask the user to continuously enter a number or type "Quit" to exit. 
+            //    The list of numbers may include duplicates.
+            //    Display the unique numbers that the user has entered.
+
+
+
+            //5 - Write a program and ask the user to supply a list of comma separated numbers(e.g 5, 1, 9, 2, 10). 
+            //    If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try;
+            //    otherwise, display the 3 smallest numbers in the list.
+
+
         }
     }
 }
