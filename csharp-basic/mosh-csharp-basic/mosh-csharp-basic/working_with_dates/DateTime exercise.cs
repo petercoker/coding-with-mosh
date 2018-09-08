2aaba9c9ea70b0f479de75b9766bb13f23eb3bbe
Peter Coker <petercoker@protonmail.com>
2018-09-08 11:44:21 +0100

    Finished excerise

--- csharp-basic/mosh-csharp-basic/mosh-csharp-basic/working_with_dates/DateTime exercise.cs
+++ csharp-basic/mosh-csharp-basic/mosh-csharp-basic/working_with_dates/DateTime exercise.cs
@@ -10,10 +10,43 @@ namespace mosh_csharp_basic.working_with_dates
 
             //Write a C# Sharp program to extract the Date property and display the DateTime value in the formatted output.
 
+            //DateTime dateTime = new DateTime();
+
+            //DateTime onPoint = DateTime.Now;
+
+            //Console.WriteLine(onPoint.ToString("D"));
+
+            //DateTime dt1 = new DateTime(2016, 6, 8, 11, 49, 0);
+            //Console.WriteLine("Complete date: " + dt1.ToString());
+
+            //Console.WriteLine();
+            //DateTime today = new DateTime(2018, 08, 09, 11, 27, 00);
+            //Console.WriteLine("Complete Today Date and Time: " + today.ToString());
+
+            // Get date-only portion of date, without its time.
+            //DateTime dateOnly = dt1.Date;
+
+            //Console.WriteLine();
+            //DateTime todayDateOnly = today.Date;
+
+            //Console.WriteLine("Short Date: " + dateOnly.ToString("d"));
+
+            //Console.WriteLine();
+            //Console.WriteLine("Short Date: " + todayDateOnly.ToString("d"));
+
+            //Console.WriteLine("Display date using 24-hour clock format:");
+
+            //Console.WriteLine(dateOnly.ToString("g"));
+            //Console.WriteLine(dateOnly.ToString("MM/dd/yyyy HH:mm"));
+
+            //Console.WriteLine();
+            //Console.WriteLine(todayDateOnly.ToString("g"));
+            //Console.WriteLine(todayDateOnly.ToString("MM/dd/yyyy HH:mm"));
+
 
 
             //C# Sharp DateTime: Exercise-7 with Solution
-            
+
             //Write a C# Sharp program to get the time of day from a given array of date time values.
 
             //C# Sharp Exercises: Time of day from a given array of date time values 
