064e3f19914ace72a3d54c1bbb26fa15f61c701b
Peter Coker <petercoker@protonmail.com>
2018-09-09 12:19:55 +0100

    finish exercise on w3resource

--- csharp-basic/mosh-csharp-basic/mosh-csharp-basic/working_with_dates/DateTime exercise.cs
+++ csharp-basic/mosh-csharp-basic/mosh-csharp-basic/working_with_dates/DateTime exercise.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace mosh_csharp_basic.working_with_dates
 {
@@ -50,6 +51,41 @@ namespace mosh_csharp_basic.working_with_dates
             //Write a C# Sharp program to get the time of day from a given array of date time values.
 
             //C# Sharp Exercises: Time of day from a given array of date time values 
+
+            //string[] timeArray = new String[3]{ "09/09/2018", "10/09/2018", "11/09/2018" };
+            //DateTime time = new DateTime();
+
+            //for (int i = 0; i < timeArray.Length; i++)
+            //{
+            //    time = DateTime.Parse(timeArray[i]);
+
+            //    Console.WriteLine(time);
+            //}
+
+            //DateTime[] dates = {DateTime.Now, new DateTime(2016, 8, 16, 9, 28, 0), new DateTime(2011, 8, 16, 9, 28, 0), new DateTime(2009, 8, 16, 9, 28, 0)};
+
+            //foreach (var date in dates)
+            //{
+            //    Console.WriteLine("Day: {0:d} Time of Day: {1:g}", date.Date, date.TimeOfDay);
+            //    Console.WriteLine("{0:d} Time: {0:t}\n", date);
+            //}
+
+            //DateTime localDate = DateTime.Now;
+            //DateTime utcDate = DateTime.UtcNow;
+            //String[] cultureNames = { "en-JM", "en-NZ", "fr-BE","de-CH", "nl-NL" };
+
+            //foreach (var cultureName in cultureNames)
+            //{
+            //    CultureInfo culture = new CultureInfo(cultureName);
+            //    Console.WriteLine("{0}:", culture.NativeName);
+
+            //    Console.WriteLine("   Local date and time: {0}, {1:G}", localDate.ToString(culture), localDate.Kind);
+            //    Console.WriteLine("   UTC date and time: {0}, {1:G}\n", utcDate.ToString(culture), utcDate.Kind);
+            //}
+
+
+
+
 
         }
     }
--- csharp-basic/mosh-csharp-basic/mosh-csharp-basic/working_with_dates/TimeSpan exercise.cs
+++ csharp-basic/mosh-csharp-basic/mosh-csharp-basic/working_with_dates/TimeSpan exercise.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Globalization;
 
 namespace mosh_csharp_basic.working_with_dates
 {
     class timespan_exercise
     {
+        
         public void timeSpanExercise()
         {
             //C# Sharp Exercises: Display the number of ticks TimeSpan object using the Ticks property
@@ -12,9 +14,10 @@ namespace mosh_csharp_basic.working_with_dates
             //and to instantiate a TimeSpan object using the Ticks property.
 
             //Note: The TimeSpan object is then used to display the elapsed time using several other time intervals.
-
-
+            
+            
 
         }
     }
 }
+
