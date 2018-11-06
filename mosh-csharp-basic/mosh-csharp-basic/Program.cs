007b86d418b99a1b77cb0b787c6dea4b52c308ab
Peter Coker <petercoker@protonmail.com>
2018-11-06 12:00:10 +0000

    completed mosh beginner course

--- mosh-csharp-basic/mosh-csharp-basic/Program.cs
+++ mosh-csharp-basic/mosh-csharp-basic/Program.cs
@@ -1,23 +1,8 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
-using System.IO; //Used for file and FileInfo Class
-using mosh_csharp_basic.primitive;
-using mosh_csharp_basic.nonprimitive;
-using mosh_csharp_basic.nonprimitive.classes;
-using mosh_csharp_basic.control_flow;
-using mosh_csharp_basic.control_flow.condition_exercises;
-using mosh_csharp_basic.control_flow.Iteration_statements;
-using mosh_csharp_basic.arrys_and_lists;
-using mosh_csharp_basic.working_with_dates;
-using mosh_csharp_basic.working_with_files;
-using mosh_csharp_basic.working_with_files.file_and_fileInfo;
-using mosh_csharp_basic.working_with_text;
-using mosh_csharp_basic.working_with_text.exercise_with_working_with_text;
-using mosh_csharp_basic.working_with_text.procedural_programming;
-using mosh_csharp_basic.working_with_files.Directory_and_DirectoryInfo;
-using mosh_csharp_basic.working_with_files.path;
-using mosh_csharp_basic.working_with_files.working_with_files_exercises;
+using System;
+using mosh_csharp_basic.Debugging;
 
 namespace mosh_csharp_basic
 {
@@ -25,11 +10,8 @@ namespace mosh_csharp_basic
     {
         static void Main(string[] args)
         {
-            //filesex1 filesex1 = new filesex1();
-            //filesex1.GetFileEx1();
-
-            filesex2 filesex2 = new filesex2();
-            filesex2.GetFileEx2();
+            CallStackWindow callStackWindow = new CallStackWindow();
+            callStackWindow.GetStack();
         }
 
     }
