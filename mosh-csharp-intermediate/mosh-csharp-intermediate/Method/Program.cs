7ad3da91437c7f0c866b94b8868fa47268e65782
Peter Coker <petercoker@protonmail.com>
2018-11-08 09:27:13 +0000

    methods

---
+++ mosh-csharp-intermediate/mosh-csharp-intermediate/Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mosh_csharp_intermediate.Method
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            //point.Move(new Point(40, 60));
+            point.Move(null);
+
+            Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+
+            point.Move(100, 200);
+            Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+        }
+    }
+}
