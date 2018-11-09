6e30d7764ef560542876c38a3abe8d3692c22037
Peter Coker <petercoker@protonmail.com>
2018-11-09 07:22:53 +0000

    completed access modifiers

---
+++ mosh-csharp-intermediate/mosh-csharp-intermediate/AccessModifiers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mosh_csharp_intermediate.AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(1994, 5, 22));
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
