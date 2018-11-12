0b694c37fc13061a878b57ebcd433d11d7d270ad
Peter Coker <petercoker@protonmail.com>
2018-11-12 07:53:53 +0000

    first init property

---
+++ mosh-csharp-intermediate/mosh-csharp-intermediate/Property/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mosh_csharp_intermediate.Property
+{
+    class Person
+    {
+        public DateTime Birthdate { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                TimeSpan timespan = DateTime.Today - Birthdate;
+                int years = timespan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ mosh-csharp-intermediate/mosh-csharp-intermediate/Property/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace mosh_csharp_intermediate.Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Birthdate = new DateTime(1982, 1, 1);
+        }
+    }
+}
